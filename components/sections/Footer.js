2cf6fc68ef76e2b564adec0a8c3d8cd372783cc7
Ian Powell <ianpowell77@yahoo.com>
2021-01-27 07:05:12 -0500

    removes navigation/social media links from footer, adds them to newsletter component

--- components/sections/Footer.js
+++ components/sections/Footer.js
@@ -1,48 +1,5 @@
 const Footer = () => {
-  return (
-    <div>
-      {/* Probably add pages/links & social-media/links to state and pull from there */}
-      <nav>
-        <ul>
-          <li>
-            <a href="">Our Work</a>
-          </li>
-          <li>
-            <a href="">Industries</a>
-          </li>
-          <li>
-            <a href="">About Us</a>
-          </li>
-          <li>
-            <a href="">Our Expertise</a>
-          </li>
-        </ul>
-      </nav>
-
-      <ul>
-        <li>
-          <a href="">
-            <img src="" alt="" />
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <img src="" alt="" />
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <img src="" alt="" />
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <img src="" alt="" />
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Footer;
--- components/sections/Newsletter.js
+++ components/sections/Newsletter.js
@@ -7,6 +7,47 @@ const Newsletter = ({ headline, text, ctaText }) => {
       <label htmlFor="email"></label>
       <input type="email" name="email" id="email" />
       <Button text={ctaText} />
+
+      {/* Probably add pages/links & social-media/links to state and pull from there */}
+      <nav>
+        <ul>
+          <li>
+            <a href="">Our Work</a>
+          </li>
+          <li>
+            <a href="">Industries</a>
+          </li>
+          <li>
+            <a href="">About Us</a>
+          </li>
+          <li>
+            <a href="">Our Expertise</a>
+          </li>
+        </ul>
+      </nav>
+
+      <ul>
+        <li>
+          <a href="">
+            <img src="" alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <img src="" alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <img src="" alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <img src="" alt="" />
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
