60bbe226fe43875c9c0890b8d960f907c133c792
Ian Powell <ianpowell77@yahoo.com>
2021-01-27 07:03:51 -0500

    Adds components to index.js/renames navigation to header

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Container from "@material-ui/core/Container";
+import Header from "../components/sections/Header";
 import Hero from "../components/sections/Hero";
+import Projects from "../components/sections/Projects";
+import Articles from "../components/sections/Articles";
+import Expertise from "../components/sections/Expertise";
+import Contact from "../components/sections/Newsletter";
+import Footer from "../components/sections/Footer";
 
 export default function Home() {
   return (
@@ -10,14 +16,22 @@ export default function Home() {
         <title>Union Site Homepage</title>
       </Head>
 
+      <Header />
+
       <main className={styles.main}>
         <Container>
           <Hero
             headline="We design &amp; build meaningful spaces"
             ctaText="Our work"
           />
+          <Projects />
+          <Articles />
+          <Expertise />
+          <Contact />
         </Container>
       </main>
+
+      <Footer />
 
       <footer className={styles.footer}></footer>
     </div>
